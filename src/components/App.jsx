4f3f800957f126f3cfc457c5dc608efab458ab9b
Olena-Rudnieva <rudnievao@gmail.com>
2023-07-18 22:46:00 +0300

    Add total function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,30 @@
+import { Component } from 'react';
 import Feedback from './Feedback/Feedback';
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  render() {
+    return (
+      <div>
+        <Feedback
+          props={this.state}
+          handleClick={this.handleClick}
+          total={this.countTotalFeedback()}
+        />
+      </div>
+    );
+  }
+}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,41 +1,39 @@
-import { Component } from 'react';
+const Feedback = ({ props, handleClick, total }) => {
+  const { good, neutral, bad } = props;
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  handleClickGood = () =>
-    this.setState(prevState => ({ good: prevState.good + 1 }));
-
-  handleClickNeutral = () =>
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-
-  handleClickBad = () =>
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-
-  render() {
-    return (
-      <div>
-        <h3>Please leave feedback</h3>
-        <button onClick={this.handleClickGood}>Good</button>
-        <button onClick={this.handleClickNeutral}>Neutral</button>
-        <button onClick={this.handleClickBad}>Bad</button>
-        <h2>Statistics</h2>
-        <p>
-          Good: <span>{this.state.good}</span>{' '}
-        </p>
-        <p>
-          Neutral: <span>{this.state.neutral}</span>
-        </p>
-        <p>
-          Bad:<span>{this.state.bad}</span>
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h3>Please leave feedback</h3>
+      {Object.keys(props).map(option => {
+        console.log(props);
+        return (
+          <button
+            key={option}
+            type="button"
+            onClick={() => handleClick(option)}
+          >
+            {option}
+          </button>
+        );
+      })}
+      <h2>Statistics</h2>
+      <p>
+        Good: <span>{good}</span>
+      </p>
+      <p>
+        Neutral: <span>{neutral}</span>
+      </p>
+      <p>
+        Bad: <span>{bad}</span>
+      </p>
+      <p>
+        Total: <span>{total}</span>
+      </p>
+      <p>
+        Positive Feedback: <span>{}</span>%
+      </p>
+    </div>
+  );
+};
 
 export default Feedback;
